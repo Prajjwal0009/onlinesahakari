c4463991b5a01508833128856d9585308a20736a
Prajjwal0009 <prajjwalsapkota12@gmail.com>
2021-04-24 09:26:00 +0545

    collector contact added

--- src/main/java/repo/CollectorRepository.java
+++ src/main/java/repo/CollectorRepository.java
@@ -10,25 +10,26 @@ public class CollectorRepository extends BaseRepository<CollectorModel> {
 
     @Override
     public String getInsertStatement(CollectorModel model){
-        return  "INSERT INTO collector(id,name,address)" +
-                "VALUES ("+model.id + ",'"+ model.name +"','"+ model.address + "')";
+        return  "INSERT INTO collector(id,name,address,contact)" +
+                "VALUES ("+model.id + ",'"+ model.name +"','"+ model.address + "','"+ model.contact + "')";
     }
 
     @Override
     public String getUpdateStatement(CollectorModel model){
         return  "UPDATE collector SET name = '" + model.name + "',"+
                 "address='"+ model.address +"'" +
+                "contact='"+ model.contact +"'" +
                 " WHERE id = "+ model.id + "";
     }
 
     @Override
     public String getSelectStatement() {
-        return "SELECT id,name,address from collector";
+        return "SELECT id,name,address,contact from collector";
     }
 
     @Override
     public String getSingleSelectStatement(int id) {
-        return "SELECT id, name,address from collector WHERE id="+ id;
+        return "SELECT id, name,address,contact from collector WHERE id="+ id;
     }
 
     @Override
@@ -42,6 +43,7 @@ public class CollectorRepository extends BaseRepository<CollectorModel> {
         collector.setId(rs.getInt("id"));
         collector.setName(rs.getString("name"));
         collector.setAddress(rs.getString("address"));
+        collector.setContact(rs.getString("contact"));
         return collector;
     }
 
